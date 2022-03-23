f5d8b86cff42514b11234dd92d305d196fcaa054
Rafael Soares de Almeida <93219825+rafadealmeida@users.noreply.github.com>
2022-03-23 17:31:41 -0300

    Testando input checkbox para dark mode

--- JS/darkMode.js
+++ JS/darkMode.js
@@ -1,5 +1,6 @@
 const $html = document.querySelector('html');
 const $switch = document.querySelector('#darkmode--checked');
+const $switchcheck = document.querySelector('#cabecalho__darkmode--checkbox');
 
 $switch.addEventListener('click', function(){
     console.log('trocou');
