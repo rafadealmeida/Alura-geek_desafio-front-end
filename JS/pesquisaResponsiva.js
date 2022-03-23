f132554b88cae981d198c032ed347888e52b5a0d
Rafael Soares de Almeida <93219825+rafadealmeida@users.noreply.github.com>
2022-03-23 00:25:52 -0300

    Barra de pesquisa responsiva
    1- Coloquei uma div insivisel no html
    2-Estilizei essa div, para ser o campo de busca, aumentando o tamanho da input
    3-No JS, eu usava o escutador de evento para sumir com os itens , por meio de um foreach, o itens indesejados
    4- No novo item coloquei um botao para sair da pesquisa, assim os itens sairiam da tela e retornaria os itens anteriores do layout

--- JS/pesquisaResponsiva.js
+++ JS/pesquisaResponsiva.js
@@ -2,18 +2,34 @@ var campobusca = document.querySelector('.cabecalho__campobusca');
 var barraPesquisa = document.querySelector('.cabecalho__barradepesquisa');
 var botaoLogin = document.querySelector('.cabecalho__botaologin');
 var lupa = document.querySelector('.cabecalho__campobusca >button');
+var darkMode = document.querySelector('#darkmode--checked');
+var logo = document.querySelector('.cabecalho__logo');
+var sair = document.querySelector('.responsive__input--x')
+var barraPesquisaResponsiva = document.querySelector('.cabecalho__campobusca--responsive')
 
+console.log(logo);
 var larguraTela = window.screen.width;
 
+const itensInvisiveis = [botaoLogin,lupa,darkMode,logo,campobusca];
 
 
 if(larguraTela<=480){
     barraPesquisa.classList.add('invisivel');
     campobusca.addEventListener('click', function(){
-        botaoLogin.classList.add('invisivel');
-        barraPesquisa.classList.remove('invisivel');
-        lupa.classList.add('invisivel');
+        itensInvisiveis.forEach(function(item,i){
+            item.classList.add('invisivel')
+        })
         
+        barraPesquisaResponsiva.classList.remove('invisivel');
+        sair.classList.remove('invisivel');
+    })
+
+    sair.addEventListener('click', function(){
+        itensInvisiveis.forEach(function(item,i){
+            item.classList.remove('invisivel')
+            barraPesquisaResponsiva.classList.add('invisivel');
+            sair.classList.add('invisivel');
+        })
     })
 }
 else{
