2ab63c4b549bf40a96282880b142d3543c02306c
Rafael Soares de Almeida <93219825+rafadealmeida@users.noreply.github.com>
2022-04-21 17:26:01 -0300

    Update app.js

--- JS/app.js
+++ JS/app.js
@@ -1,17 +1,27 @@
 import {valida} from "./valida.js"
 
+
 const inputs = document.querySelectorAll('input');
 const botao = document.querySelector('.usuariologin__botaoentrar');
 
-
-
-
-
     inputs.forEach( input => {
-       input.addEventListener('blur',evento=>{
-           valida(evento.target)
-       });
-    })
+        input.addEventListener('submit',evento=>{
+            valida(evento.target)
+            
+     })
+
+     
+     
+})
+
+
+botao.addEventListener('submit',evento=>{
+    evento.defaultPrevented()
+})
+
+
+
+   
 
    
 
