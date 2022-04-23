bee29d4f57eac8d65dad2dc53136e14c756600e7
Rafael Soares de Almeida <93219825+rafadealmeida@users.noreply.github.com>
2022-04-23 11:51:08 -0300

    Update teste_auteiticacao.js
    1-Captei o span , e coloquei um innerHTML, para modificar o conteúdo , caso haja erro nos input
    2-Adicionei logica de reconhecer erro, para uso futuro

--- JS/teste_auteiticacao.js
+++ JS/teste_auteiticacao.js
@@ -3,6 +3,8 @@ const botao = document.querySelector(".usuariologin__botaoentrar")
 const email = document.querySelector("#usuariologin__email").value
 const senha = document.querySelector("#usuariologin__senha")
 
+const mensagemErro = document.querySelector(".input__mensagem--erro")
+
 botao.addEventListener("click", evento =>{
     evento.preventDefault()
    buscaUsuario()
@@ -29,10 +31,48 @@ const verificaUsuario = async () =>{
     dados.forEach(dado =>{
         if(dado.email === email || dado.senha === senha.value){
             console.log("logou")
+            mensagemErro.innerHTML=''
             window.location.href ='../html/pagina-todosOsProdutos.html'
         }else{
             console.log("login inválido")
+            mensagemErro.innerHTML ="Email ou senha incorretos. Tente Novamente!"
         }
 
     })
+}
+
+
+const tipoDeErro = [
+    'valueMissing', 
+    'typeMissing', 
+    'customError'
+]
+
+const mensagemDeErro = {
+    nome:{
+        valueMissing:'O campo nome não pode está vazio'
+
+    },
+    email:{
+        valueMissing:'O campo email não pode está vazio',
+        typeMismatch:'O email digitado não é valido',
+        customError:'O login está incorreto! Tente novamente.'
+    },
+    senha:{
+        valueMissing:'O campo senha não pode está vazio',
+        customError:'A senha está incorreto! Tente novamente.'
+    
+    }
+}
+
+
+function mostraMensagemErro(tipoDeInput,input){
+    let mensagem = ''
+
+    tipoDeErro.forEach(erro=>{
+        if(input.validity[erro]){
+            mensagem = mensagemDeErro[tipoDeInput][erro]
+        }
+    })
+    return mensagem
 }
