679493fdc0a3ccaa9a7f9d6eff72289953406280
Rafael Soares de Almeida <93219825+rafadealmeida@users.noreply.github.com>
2022-04-28 14:49:21 -0300

    Update teste_auteiticacao.js
    Separando os input de login em inpute de email e senha, para verificar se está com o igual no json server, simulando um banco de dados.
    2-Adicionando uma classe para ser adicionado caso os inputs estiverem incorretos
    3-Criei uma função para checar os erros de input, e mostrar mensagem personalizada para cada erro para cada input,porem ainda não esta funcionando
    To do: Verificar a função de checar erro

--- JS/teste_auteiticacao.js
+++ JS/teste_auteiticacao.js
@@ -1,7 +1,11 @@
 const botao = document.querySelector(".usuariologin__botaoentrar")
 
-const email = document.querySelector("#usuariologin__email").value
-const senha = document.querySelector("#usuariologin__senha")
+const emailInput = document.querySelector("#usuariologin__email")
+const senhaInput = document.querySelector("#usuariologin__senha")
+
+const inputs = document.querySelectorAll('input')
+
+
 
 const mensagemErro = document.querySelector(".input__mensagem--erro")
 
@@ -9,6 +13,10 @@ botao.addEventListener("click", evento =>{
     evento.preventDefault()
    buscaUsuario()
    verificaUsuario()
+
+   inputs.forEach(input =>{
+       checarErro(input)
+   })
 
   
 })
@@ -29,13 +37,20 @@ const verificaUsuario = async () =>{
     const dados = await buscaUsuario()
 
     dados.forEach(dado =>{
-        if(dado.email === email || dado.senha === senha.value){
-            console.log("logou")
+        if( emailInput.value === dado.email && senhaInput.value === dado.senha){
+            
             mensagemErro.innerHTML=''
             window.location.href ='../html/pagina-todosOsProdutos.html'
+            emailInput.classList.remove('input__mensagem--erro')
+            senhaInput.classList.remove('input__mensagem--erro')
         }else{
-            console.log("login inválido")
-            mensagemErro.innerHTML ="Email ou senha incorretos. Tente Novamente!"
+            emailInput.classList.add('input__mensagem--erro')
+            senhaInput.classList.add('input__mensagem--erro')
+
+            mensagemErro.innerHTML ="Email ou senha incorreto. Tente novamente !"
+            inputs.forEach(input =>{
+                checarErro(input)
+            })
         }
 
     })
@@ -67,7 +82,9 @@ const mensagemDeErro = {
 
 
 function mostraMensagemErro(tipoDeInput,input){
-    let mensagem = ''
+    let mensagem = '';
+
+    
 
     tipoDeErro.forEach(erro=>{
         if(input.validity[erro]){
@@ -75,4 +92,19 @@ function mostraMensagemErro(tipoDeInput,input){
         }
     })
     return mensagem
+}
+
+
+function checarErro(input){
+    const tipoDeInput = input.dataset.tipo
+    
+    if(input.validity.valid){
+        input.classList.remove('usuariologin--erro')
+        document.querySelector(".input__mensagem--erro").innerHTML = " "
+    }
+    else{
+        input.classList.add('usuariologin--erro')
+        const error =  mostraMensagemErro(tipoDeInput,input)
+        console.log(error)
+    }
 }
