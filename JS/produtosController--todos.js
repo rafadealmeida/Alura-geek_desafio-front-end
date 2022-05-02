0fb43af1277f3924ac81abf8830397b6543e89df
Rafael Soares de Almeida <93219825+rafadealmeida@users.noreply.github.com>
2022-05-02 14:22:11 -0300

    Criando os produtos com a resposta da API
    1-Ultilizando de base scripts já criado para construir o produto via API com algumas modificações

---
+++ JS/produtosController--todos.js
@@ -0,0 +1,50 @@
+import { produtosService } from "./teste_api.js";
+// import { criarItemProduto } from "./produtos--controller.js"
+
+
+const gradeProdutos = document.querySelector('.produtos__grade--todososprodutos')
+
+
+const criarProdutoGrade = (nome,img,preco,id) =>{
+    const itemProduto = document.createElement("li")
+    const conteudo = `
+        
+        <a href="maisdetalhes_teste.html?id=${id}" class="produto__link"><img src="${img}" alt="">
+        <h3 class="produto__nome">${nome}</h3>
+        <p class="produto__codigo">#11111111</p>
+        <a href="maisdetalhes_teste.html?id=${id}" class="produto__link">Ver produto</a>
+        
+    `
+    itemProduto.classList.add('produto');
+    itemProduto.innerHTML = conteudo;
+    itemProduto.dataset.id = id
+    return itemProduto;
+}
+
+const renderTodosProdutos = async () =>{ 
+    
+    try {
+        const dados = await produtosService.listaProdutos()
+            
+            
+        dados.forEach(elemento=>{
+                      
+               if(gradeProdutos.children.length <= 17){
+
+                   gradeProdutos.appendChild(criarProdutoGrade(elemento.nome,elemento.img,elemento.preco,elemento.id))
+               }
+                    
+                        
+                
+               
+                                    
+        })
+
+    }
+    catch(erro){
+        console.log(erro)
+    }
+}
+
+renderTodosProdutos();
+
