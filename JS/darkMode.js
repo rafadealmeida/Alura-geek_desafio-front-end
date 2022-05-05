2aa3df299ed2d7e193827681daf3c346b7b79791
Rafael Soares de Almeida <93219825+rafadealmeida@users.noreply.github.com>
2022-05-05 16:51:55 -0300

    Retirando console log

--- JS/darkMode.js
+++ JS/darkMode.js
@@ -3,6 +3,5 @@ const $switch = document.querySelector('#darkmode--checked');
 const $switchcheck = document.querySelector('#cabecalho__darkmode--checkbox');
 
 $switch.addEventListener('click', function(){
-    console.log('trocou');
     $html.classList.toggle('darkmode');
 })
