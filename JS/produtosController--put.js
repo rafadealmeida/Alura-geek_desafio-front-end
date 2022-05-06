99952135876db2a4d8896063091b770cd75ac22e
Rafael Soares de Almeida <93219825+rafadealmeida@users.noreply.github.com>
2022-05-06 17:25:40 -0300

    Update produtosController--put.js
    1-Pegando os inputs
    2-Fazendo a requisição dos detalhes do produto
    3-Na imagem tive que utilizar a propriedade .srcset, para conseguir mudar o caminho da imagem
    4-Preparando o envio do formulário para atualizar o produto

--- JS/produtosController--put.js
+++ JS/produtosController--put.js
@@ -1,3 +1,47 @@
-const atualizaProduto = (nome,preco,descricao) =>{
-    return fetch (``)
+import { produtosService } from "./teste_api.js"
+import { abrirJanelaModal } from "./produtosController--todos.js"
+
+const botao = document.querySelector('.novoproduto__adicionar--botao')
+
+const nomeInput =document.querySelector( '[data-tipo ="nome__produto"]')
+const precoInput =document.querySelector( '[data-tipo ="preco__produto"]')
+const descricaoInput =document.querySelector( '[data-tipo ="descricao__produto"]')
+const imagemProduto = document.querySelector('.img-edição--produto')
+
+export const pegaInput = async (id) =>{
+
+    produtosService.detalhaProdutos(id).then(dados => {
+        nomeInput.value = dados.nome
+        precoInput.value = dados.preco
+        descricaoInput.value = dados.detalhe
+        imagemProduto.srcset = dados.img
+        
+    })
+    
+  
 }
+
+const eviarFormulario = ()=> {
+    await abrirJanelaModal()
+
+    botao.addEventListener('submit',(evento) => {
+        evento.preventDefault()
+        produtosService.atualizaProduto(nomeInput.value,precoInput.value,descricaoInput.value).then(() => {
+            alert("Produto atualizado com sucesso!")
+        })
+        console.log("clicou")
+    })
+
+}
+eviarFormulario()
+
+// botao.addEventListener('submit',(evento) => {
+//     evento.preventDefault()
+//     produtosService.atualizaProduto(nomeInput.value,precoInput.value,descricaoInput.value).then(() => {
+//         alert("Produto atualizado com sucesso!")
+//     })
+//     console.log("clicou")
+// })
+
+
+
